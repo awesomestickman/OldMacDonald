4219e5dacaae762858f9d7e8862568b4973ebe7c
unknown <tholsen@697-334-Boromir.sf.k12.internal>
2015-10-22 08:15:39 -0700

    finished

--- Chick.java
+++ Chick.java
@@ -1,4 +1,33 @@
 class Chick implements Animal 
 {     
-     //your code here
+     private String myType;     
+     private String mySound; 
+     private String mySound2;     
+     public Chick(String type, String sound1, String sound2)     {         
+         myType = type;         
+         mySound = sound1;     
+         mySound2=sound2;
+     }     
+     public Chick()     {         
+         myType = "unknown";         
+         mySound = "unknown";     
+     }      
+     public String getSound(){
+int randomer = (int)(Math.random()*2);
+System.out.println(randomer);
+if(randomer==0){
+     	return mySound;
+}
+else if(randomer==1){
+     	return mySound2;
+}
+else{
+	return mySound2;
+}
+
+
+
+     }     
+     public String getType(){return myType;} 
+
 }
--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,20 @@
-class NamedCow
+class NamedCow extends Cow
 {
+	private String streetName;
+
+ public NamedCow(String type,String n, String sound)     {         
+         myType = type;         
+         mySound = sound;  
+         streetName=n;
+   
+}  
+public String getName(){
+
+return(streetName);
+
+}
+
+
+
 
 }
